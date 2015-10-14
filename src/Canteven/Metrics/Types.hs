ceb779d6fb484217898ccde6aa7c2ad896506701
Giovanni Cappellotto <gcappellotto@sumall.com>
2015-10-14 18:25:56 -0400

    Make CarbonOptions keys required
    Due to a new behavior of (.:?) in Data.Aeson >0.9 CarbonOptions
    configuration values that are null fail to parse.
    
    The (.:) behave in the same way in both Data.Aeson versions: it returns
    Nothing if the value is null and it fails to parse if the key is not
    present.
    
    Usually configuration templates include all the keys and using (.:) seems
    the appropriate choice.
    
    See also https://github.com/bos/aeson/issues/287.

--- src/Canteven/Metrics/Types.hs
+++ src/Canteven/Metrics/Types.hs
@@ -6,7 +6,7 @@ module Canteven.Metrics.Types (
 ) where
 
 import Control.Applicative ((<$>), (<*>))
-import Data.Aeson (FromJSON, parseJSON, Value(Object), (.:?), (.!=))
+import Data.Aeson (FromJSON, parseJSON, Value(Object), (.:?), (.:), (.!=))
 import Data.ByteString (ByteString)
 import System.Remote.Monitoring.Carbon (CarbonOptions(CarbonOptions))
 
@@ -24,8 +24,8 @@ instance FromJSON MetricsConfig where
     case mMetrics of
       Nothing -> return defaultMetricsConfig
       Just config -> MetricsConfig
-        <$> config .:? "host" .!= "0.0.0.0"
-        <*> config .:? "port" .!= 8081
+        <$> config .: "host" .!= "0.0.0.0"
+        <*> config .: "port" .!= 8081
         <*> config .:? "carbon"
   parseJSON value =
     fail $ "Couldn't parse metrics config from " ++ show value
@@ -49,10 +49,10 @@ instance FromJSON CarbonConfig where
   parseJSON (Object config) = CarbonConfig <$> options
     where
       options = CarbonOptions
-        <$> config .:? "host" .!= "127.0.0.1"
-        <*> config .:? "port" .!= 2003
-        <*> config .:? "flushInterval" .!= 1000
-        <*> config .:? "prefix" .!= ""
-        <*> config .:? "suffix" .!= ""
+        <$> config .: "host" .!= "127.0.0.1"
+        <*> config .: "port" .!= 2003
+        <*> config .: "flushInterval" .!= 1000
+        <*> config .: "prefix" .!= ""
+        <*> config .: "suffix" .!= ""
   parseJSON value =
     fail $ "Couldn't parse Carbon config from " ++ show value
